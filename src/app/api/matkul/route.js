cd1aa3e8f17561c739d2288aed2229174e971756
GuyDimension <billycelino67@gmail.com>
2025-04-27 19:47:37 +0700

    Fitur create

--- src/app/api/matkul/route.js
+++ src/app/api/matkul/route.js
@@ -7,3 +7,19 @@ orderBy: { id: 'asc' },
 
 return new Response(JSON.stringify(data), { status: 200 });
 }
+
+export async function POST(request) {
+    const { kode, nama } = await request.json();
+
+    if (!kode || !nama) {
+        return new Response(JSON.stringify({ error: 'Semua field wajib diisi' }), {
+            status: 400,
+        });
+    }
+
+    const matkul = await prisma.matkul.create({
+        data: { kode, nama },
+    });
+
+    return new Response(JSON.stringify(matkul), { status: 201 });
+}
--- src/app/matkul/page.jsx
+++ src/app/matkul/page.jsx
@@ -5,6 +5,10 @@ import { useEffect, useState } from 'react';
 export default function MatkulPage() {
     
     const [matkuls, setMatkuls] = useState([]);
+    const [kode, setKode ] = useState('');
+    const [nama, setNama ] = useState('');
+    const [msg, setMsg ] = useState(''); 
+    const [formVisible, setFormVisible ] = useState(false);
 
     const fetchMatkuls = async () => {
         const res = await fetch('/api/matkul');
@@ -16,8 +20,58 @@ export default function MatkulPage() {
         fetchMatkuls();
     }, []);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await fetch('/api/matkul', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ kode, nama }),
+        });
+
+        if (res.ok) {
+            setMsg('Berhasil disimpan');
+            setKode('');
+            setNama('');
+            setFormVisible(false);
+            fetchMatkuls(); // refresh data
+        } else {
+            setMsg('Gagal menyimpan data');
+        }
+    };
+
     return (
         <div>
+            <button
+                onClick={() => setFormVisible(!formVisible)}>
+                {formVisible ? 'Tutup Form' : 'Tambah Matkul'}
+            </button>
+            {formVisible && (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input 
+                            type="text"
+                            value={kode}
+                            onChange={ (e) => setKode(e.target.value)}
+                            placeholder="Masukkan Kode"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            value={nama}
+                            onChange={ (e) => setNama(e.target.value)}
+                            placeholder="Masukkan Nama Matkul"
+                            required
+                        />
+                    </div>
+                    <button type="submit">
+                        Simpan
+                    </button>
+                    <p>{msg}</p>
+                </form>
+            )}
+            <br></br>
             <table border="1">
                 <thead>
                     <tr>
@@ -44,3 +98,4 @@ export default function MatkulPage() {
         </div>
     );
 }
+
